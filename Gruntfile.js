b54087cdfaf419fbe3dab3a292120da1e3ca63ed
Max Waterman <max.waterman@intel.com>
2013-10-26 10:21:16 +0100

    Adding xpk target to gruntfile, plus signature file.

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,7 +11,7 @@ module.exports = function (grunt) {
 
   grunt.initConfig({
     packageInfo: grunt.file.readJSON('package.json'),
-    chromeInfo: grunt.file.readJSON('data/manifest.json'),
+    chromeInfo: grunt.file.readJSON('data/chrome-crx/manifest.json'),
 
     clean: ['build'],
 
@@ -20,9 +20,9 @@ module.exports = function (grunt) {
       // (default: '/tmp')
       tizenAppScriptDir: '/home/developer/',
 
-      // path to the config.xml file for the Tizen wgt file
+      // path to the config.xml file for the Tizen wgt file - post templating
       // (default: 'config.xml')
-      configFile: 'data/config.xml',
+      configFile: 'build/wgt/config.xml',
 
       // path to the sdb command (default: process.env.SDB or 'sdb')
       sdbCmd: 'sdb'
@@ -32,7 +32,7 @@ module.exports = function (grunt) {
     uglify: {
       dist: {
         files: [
-          {cwd: 'app/js', src: '*.js', dest: 'build/app/js', expand: true}
+          { expand: true, cwd: '.', src: 'app/js/**/*.js', dest: 'build/' }
         ]
       }
     },
@@ -40,12 +40,9 @@ module.exports = function (grunt) {
     // minify CSS
     cssmin: {
       dist: {
-        files: {
-          'build/app/css/hangonman.css': ['app/css/hangonman.css'],
-          'build/app/css/figure.css': ['app/css/figure.css'],
-          'build/app/css/license.css': ['app/css/license.css'],
-          'build/app/css/start.css': ['app/css/start.css']
-        }
+        files: [
+          { expand: true, cwd: '.', src: ['app/css/**/*.css'], dest: 'build/' }
+        ]
       }
     },
 
@@ -53,36 +50,89 @@ module.exports = function (grunt) {
       common: {
         files: [
           { expand: true, cwd: '.', src: ['app/lib/**'], dest: 'build/' },
-          { expand: true, cwd: '.', src: ['app/images/*.svg'], dest: 'build/' },
-          { expand: true, cwd: '.', src: ['app/images/*.xcf'], dest: 'build/' },
           { expand: true, cwd: '.', src: ['app/audio/**'], dest: 'build/' },
-          { expand: true, cwd: '.', src: ['app/data/**'], dest: 'build/' },
           { expand: true, cwd: '.', src: ['LICENSE'], dest: 'build/app/' },
           { expand: true, cwd: '.', src: ['README.txt'], dest: 'build/app/' },
-          { expand: true, cwd: '.', src: ['app/_locales/**'], dest: 'build/' }
+          { expand: true, cwd: '.', src: ['app/_locales/**'], dest: 'build/' },
+          { expand: true, cwd: '.', src: ['app/data/**'], dest: 'build/' },
+          { expand: true, cwd: '.', src: ['app/images/*.svg'], dest: 'build/' },
+          { expand: true, cwd: '.', src: ['app/images/*.xcf'], dest: 'build/' }
         ]
       },
+
       wgt: {
         files: [
           { expand: true, cwd: 'build/app/', src: ['**'], dest: 'build/wgt/' },
-          { expand: true, cwd: 'data/', src: ['config.xml'], dest: 'build/wgt/' },
           { expand: true, cwd: '.', src: ['icon_128.png'], dest: 'build/wgt/' }
         ]
       },
+
+      wgt_config: {
+        files: [
+          { expand: true, cwd: 'data/tizen-wgt/', src: ['config.xml'], dest: 'build/wgt/' }
+        ],
+        options:
+        {
+          processContent: function(content, srcpath)
+          {
+            return grunt.template.process(content);
+          }
+        }
+      },
+
       crx: {
         files: [
           { expand: true, cwd: 'build/app/', src: ['**'], dest: 'build/crx/' },
-          { expand: true, cwd: '.', src: ['manifest.json'], dest: 'build/crx/' },
           { expand: true, cwd: '.', src: ['icon*.png'], dest: 'build/crx/' }
         ]
       },
+
+      crx_manifest:
+      {
+        files: [
+          { expand: true, cwd: 'data/chrome-crx/', src: ['manifest.json'], dest: 'build/crx/' }
+        ],
+
+        options:
+        {
+          processContent: function(content, srcpath)
+          {
+            return grunt.template.process(content);
+          }
+        }
+
+      },
+
+      xpk: {
+        files: [
+          { expand: true, cwd: 'build/app/', src: ['**'], dest: 'build/xpk/' },
+          { expand: true, cwd: '.', src: ['icon*.png'], dest: 'build/xpk/' }
+        ]
+      },
+
+      xpk_manifest:
+      {
+        files: [
+          { expand: true, cwd: 'data/tizen-xpk/', src: ['manifest.json'], dest: 'build/xpk/' }
+        ],
+
+        options:
+        {
+          processContent: function(content, srcpath)
+          {
+            return grunt.template.process(content);
+          }
+        }
+
+      },
+
       sdk: {
         files: [
           { expand: true, cwd: 'build/app/', src: ['**'], dest: 'build/sdk/' },
           { expand: true, cwd: 'app/', src: ['js/**'], dest: 'build/sdk/' },
           { expand: true, cwd: 'app/', src: ['css/**'], dest: 'build/sdk/' },
           { expand: true, cwd: 'app/', src: ['*.html'], dest: 'build/sdk/' },
-          { expand: true, cwd: 'data/', src: ['config.xml'], dest: 'build/sdk/' },
+          { expand: true, cwd: 'data/tizen-wgt/', src: ['config.xml'], dest: 'build/sdk/' },
           { expand: true, cwd: '.', src: ['icon*.png'], dest: 'build/sdk/' }
         ]
       }
@@ -155,7 +205,7 @@ module.exports = function (grunt) {
       install: {
         action: 'install',
         remoteFiles: {
-          pattern: '/home/developer/HangOnMan*.wgt',
+          pattern: '/home/developer/<%= packageInfo.name %>*.wgt',
           filter: 'latest'
         }
       },
@@ -192,14 +242,15 @@ module.exports = function (grunt) {
     'copy:common'
   ]);
 
-  grunt.registerTask('crx', ['dist', 'copy:crx']);
-  grunt.registerTask('wgt', ['dist', 'copy:wgt', 'package:wgt']);
-
+  grunt.registerTask('crx', ['dist', 'copy:crx', 'copy:crx_manifest']);
+  grunt.registerTask('wgt', ['dist', 'copy:wgt', 'copy:wgt_config', 'package:wgt']);
+  grunt.registerTask('xpk', ['dist', 'copy:xpk', 'copy:xpk_manifest']);
   grunt.registerTask('sdk', [
     'clean',
     'imagemin:dist',
     'copy:common',
     'copy:sdk',
+    'copy:wgt_config',
     'package:sdk'
   ]);
 
@@ -208,6 +259,7 @@ module.exports = function (grunt) {
     'uglify:perf',
     'inline',
     'copy:wgt',
+    'copy:wgt_config',
     'package:wgt'
   ]);
 
